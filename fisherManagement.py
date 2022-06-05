0595edf1d8c5275c5159e5656f2b6286485374bf
mantasreika <mantas.reika@presentconnection.eu>
2022-06-05 20:52:01 +0300

    information message

--- fisherManagement.py
+++ fisherManagement.py
@@ -16,22 +16,18 @@ class FisherManagement():
     def registerFisher(self, name: str, category: str) -> None:
         fisher = Fisher(name, FisherCategory[category])
         self.fishers.append(fisher)
+        print('Fisher registered!\n')
 
     def registerFish(self, fisherId: int, name: str, weight: float) -> None:
         try:
             self.fishers[fisherId].addFish(Fish(name, weight))
         except:
             print('Could not add a fish, check data and try again...')
+            return
+        print('Fish added!\n')
 
     def findWinner(self) -> Fisher:
-        # winner: Fisher = None
         return max(self.fishers, key=lambda x: x.getScore())
-        # for i in self.fishers:
-        #     if (winner == None):
-        #         winner = i
-        #     elif (winner.getScore() < i.getScore()):
-        #         winner = i
-        # return winner
 
     def findTopFishers(self) -> List[Fisher]:
         return sorted(self.fishers, key=lambda x: x.getScore(), reverse=True)
